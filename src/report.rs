8e6a7bdd7b066f1871c75d2b575ec1e5eeb7decf
Koen Zandberg <koen@bergzand.net>
2026-03-05 16:55:21 +0100

    tests(report): add simple test cases

--- src/report.rs
+++ src/report.rs
@@ -42,3 +42,36 @@ impl<'b, C> Decode<'b, C> for ReportingPolicy {
         Ok(ReportingPolicy::new(policy))
     }
 }
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+mod tests {
+    extern crate std;
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let policy = ReportingPolicy::new(0u8);
+        assert!(!policy.send_record_on_success());
+        assert!(!policy.send_record_on_failure());
+        assert!(!policy.add_sysinfo_on_success());
+        assert!(!policy.add_sysinfo_on_failure());
+    }
+
+    #[test]
+    fn all() {
+        let policy = ReportingPolicy::new(15u8);
+        assert!(policy.send_record_on_success());
+        assert!(policy.send_record_on_failure());
+        assert!(policy.add_sysinfo_on_success());
+        assert!(policy.add_sysinfo_on_failure());
+    }
+
+    #[test]
+    fn not_a_report_policy() {
+        let input = std::vec![0x20];
+        let mut decoder = minicbor::Decoder::new(&input);
+        let res = decoder.decode::<ReportingPolicy>().unwrap_err();
+        assert!(res.is_type_mismatch());
+    }
+}
