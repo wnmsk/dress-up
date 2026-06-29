032770a92a0cb7150070e6a44478a42b447f44ae
wnmsk <61439328+wnmsk@users.noreply.github.com>
2026-06-29 12:30:50 +0200

    try earlier toolchain

--- fuzz/scripts/fuzz_cov.sh
+++ fuzz/scripts/fuzz_cov.sh
@@ -20,7 +20,7 @@ REPORT_DIR="fuzz/results/cov_reports/$(date +%Y-%m-%d)"
 # --> https://github.com/rust-fuzz/cargo-fuzz/issues/308
 # LLVM_COV_BIN="${HOME}/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov"
 # LLVM_COV_BIN="$(rustup which llvm-cov)" # trying to get the correct binary
-LLVM_COV_BIN="${HOME}/.rustup/toolchains/nightly-2026-06-01-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov"
+LLVM_COV_BIN="${HOME}/.rustup/toolchains/nightly-2026-03-01-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov"
 
 # create report directory
 mkdir -p "${REPORT_DIR}";
