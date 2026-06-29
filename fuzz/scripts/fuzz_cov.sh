7598e899b35db2349e4fb202d19199e857e56cec
wnmsk <61439328+wnmsk@users.noreply.github.com>
2026-06-29 13:56:37 +0200

    pin nightly toolchain

--- fuzz/scripts/fuzz_cov.sh
+++ fuzz/scripts/fuzz_cov.sh
@@ -13,14 +13,17 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
+: "${NIGHTLY_TOOLCHAIN:=nightly}"
+
+echo "Using toolchain: ${NIGHTLY_TOOLCHAIN}"
+
 TARGET="$1"
 REPORT_DIR="fuzz/results/cov_reports/$(date +%Y-%m-%d)"
 
 # use direct binary path since 'cargo cov' doesn't seem to work on the VM
 # --> https://github.com/rust-fuzz/cargo-fuzz/issues/308
-# LLVM_COV_BIN="${HOME}/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov"
-# LLVM_COV_BIN="$(rustup which llvm-cov)" # trying to get the correct binary
-LLVM_COV_BIN="${HOME}/.rustup/toolchains/nightly-2026-03-01-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov"
+LLVM_COV_BIN="${HOME}/.rustup/toolchains/${NIGHTLY_TOOLCHAIN}-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-cov"
+
 
 # create report directory
 mkdir -p "${REPORT_DIR}";
