1de22bb3c76343f3b99d953e79c40aee6fb6999b
wnmsk <61439328+wnmsk@users.noreply.github.com>
2026-07-13 13:21:26 +0200

    test: add fuzzing targets

---
+++ fuzz/src/os_hooks.rs
@@ -0,0 +1,103 @@
+use dress_up::{error::Error, OperatingHooks};
+use std::cell::Cell;
+use uuid::Uuid;
+
+// OsHooks copied from examples/minimal/main.rs
+pub struct OsHooks<'a> {
+    payload: &'a [u8],
+    storage: Cell<Vec<u8>>,
+    capacity: usize,
+    vendor_id: Uuid,
+    class_id: Uuid,
+}
+
+impl<'a> OsHooks<'a> {
+    pub fn new(capacity: usize, vendor_id: Uuid, class_id: Uuid, payload: &'a [u8]) -> Self {
+        Self {
+            payload,
+            storage: Cell::new(Vec::with_capacity(capacity)),
+            capacity,
+            vendor_id,
+            class_id,
+        }
+    }
+}
+
+impl<'a> OperatingHooks for OsHooks<'a> {
+    type ReadWriteBufferSize = generic_array::typenum::U64;
+
+    fn match_vendor_id(
+        &self,
+        uuid: Uuid,
+        _component: &dress_up::component::Component,
+    ) -> Result<bool, dress_up::error::Error> {
+        Ok(self.vendor_id == uuid)
+    }
+
+    fn match_class_id(
+        &self,
+        uuid: Uuid,
+        _component: &dress_up::component::Component,
+    ) -> Result<bool, dress_up::error::Error> {
+        Ok(self.class_id == uuid)
+    }
+
+    fn component_read(
+        &self,
+        _component: &dress_up::component::Component,
+        _slot: Option<u64>,
+        offset: usize,
+        bytes: &mut [u8],
+    ) -> Result<(), dress_up::error::Error> {
+        let storage = self.storage.take();
+        if bytes.len() + offset > storage.len() {
+            self.storage.set(storage);
+            return Err(Error::InvalidCommandSequence { position: 0 });
+        }
+        bytes.copy_from_slice(&storage[offset..offset + bytes.len()]);
+        self.storage.set(storage);
+        Ok(())
+    }
+
+    fn component_write(
+        &self,
+        _component: &dress_up::component::Component,
+        _slot: Option<u64>,
+        _offset: usize,
+        _bytes: &[u8],
+    ) -> Result<(), dress_up::error::Error> {
+        // return Error instead of todo!()-macro to prevent fuzzer from crashing
+        // TODO: maybe implement minimal logic
+        Err(dress_up::error::Error::EndOfInput)
+    }
+
+    fn component_size(
+        &self,
+        _component: &dress_up::component::Component,
+    ) -> Result<usize, dress_up::error::Error> {
+        let storage = self.storage.take();
+        let len = storage.len();
+        self.storage.set(storage);
+        Ok(len)
+    }
+
+    fn component_capacity(
+        &self,
+        _component: &dress_up::component::Component,
+    ) -> Result<usize, dress_up::error::Error> {
+        Ok(self.capacity)
+    }
+
+    fn fetch(
+        &self,
+        _component: &dress_up::component::Component,
+        _slot: Option<u64>,
+        _uri: &str,
+    ) -> Result<(), Error> {
+        let mut storage = self.storage.take();
+        storage.clear();
+        storage.extend_from_slice(self.payload);
+        self.storage.set(storage);
+        Ok(())
+    }
+}
